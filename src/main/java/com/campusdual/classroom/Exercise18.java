da99dcc5158bd2cdf4d4d06588bab5dd4cad029f
Hugo <hugonvsg@gmail.com>
2024-11-22 08:52:56 +0100

    ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -4,5 +4,26 @@ public class Exercise18 {
 
 	public static void main(String[] args) {
 
+		showInlineArray(createAndInitializeArray(5));
+	}
+
+    public static int[] createAndInitializeArray(int n) {
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+        return arr;
+    }
+
+	public static void showInlineArray(int[] arr) {
+        for (int i : arr) {
+			if (i == arr.length){
+				System.out.println(i);
+			}else{
+				System.out.print(i+" ");
+			}
+
+        }
 	}
 }
+
